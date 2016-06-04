9aabf3458cedd11a5d48824d34f0d92caa9e8a91
Tannin <sherb@gmx.net>
2016-06-04 19:45:39 +0200

    translation

---
+++ src/configurator_en.ts
@@ -0,0 +1,27 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>PyCfgDialog</name>
+    <message>
+        <location filename="pyCfgDialog.ui" line="14"/>
+        <source>Configurator</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="pyCfgDialog.ui" line="48"/>
+        <source>Basic</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="pyCfgDialog.ui" line="113"/>
+        <source>Save</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="pyCfgDialog.ui" line="120"/>
+        <source>Close</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
